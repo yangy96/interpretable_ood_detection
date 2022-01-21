82afd3377a43dbd1e6a6ccc71ae696cadfc93708
yangy96 <yangy96@stanford.edu>
2022-01-20 23:15:33 -0500

    minor changes

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
 # Artifact Evaluation 
-artifactual evaluation for *Interpretable Detection of Distribution Shifts in Learning Enabled Cyber-Physical Systems*
+artifact evaluation for *Interpretable Detection of Distribution Shifts in Learning Enabled Cyber-Physical Systems*
 
 ## Firstly, please download all necessary data files to the place
 
 `cd carla_experiments`
 
-put a download link here 
+TO DO : put a download link here 
 
 `cd ../`
 
 `cd carla_adversarial_experiments`
 
-put a download link here 
+TO DO: put a download link here 
 
 `cd ../`
 
 ### Environment set up
 
-To build a docker image: `docker build -t reproduce-test .`
+#### Docker
+To build a docker image: `docker build -t reproduce-test .` <br>
 To run the docker container and open an interactive session with docker: `docker run -i -t --gpus all --name temp_test --rm reproduce-test /bin/bash`
 
-if succeed, reproduce the following experiments in docker container 
+If succeed, reproduce the following experiments in docker container 
 
-if any occur with docker instatiation, could also run <br>
+#### Virtual Environment 
+If there is any error occur with docker instantiation, could also run <br>
 - `python3 -m venv env`
 - `source env/bin/activate`
 - `pip install -r requirements.txt`
 and reproduce following experiments <br>
 
-After finishing the experiments, <br>
+After finishing the experiments, to leave the virtual environment <br>
 if using Docker container, `exit` <br>
 if using venv, `deactivate`
 
@@ -37,7 +39,7 @@ run `cd carla_experiments` <br>
 more details could be found in README.md in the carla_experiments 
 
 ## To run OOD experiments to detect perturbations by adversarial attack (section 6.3)
-run  `cd carla_adversarial_experiments` <br>
+run `cd carla_adversarial_experiments` <br>
 more details could be found in README.md in the carla_adversarial_experiments
 
 ## To run OOD experiments for lidar data (section 7.3)
--- lidar_experiments/memories/form_memories.py
+++ lidar_experiments/memories/form_memories.py
@@ -29,14 +29,16 @@ def run_crash_prediction(memory_dir, source_dir, window_size, prob_threshold, wi
     dist = memory_dir.split("/")[-1].split("_")[-1]
     stats = compute_crash_prediction_accuracy(source_dir, memorization_object,window_size, window_threshold, prob_threshold)
     #print("Stats computed - ", stats)
-    print("(W: %s tau: %s alpha: %s dist: %s) " % (str(window_size),str(window_threshold),str(prob_threshold),str(dist)))
     f = open("./results/lidar_exp_results.txt", "a")
+    print("(W: %s tau: %s alpha: %s dist: %s) " % (str(window_size),str(window_threshold),str(prob_threshold),str(dist)))
+    f.write("(W: {} tau: {} alpha: {} dist: {} ) \n".format(str(window_size),str(window_threshold),str(prob_threshold),str(dist)))
+    
     if (len(stats["alarm time ahead"]) > 0):
         print(("TPR: %f FPR: %f MPR: %f Avg Forecast: %f ") %( stats["correct_prediction_percent"], stats["false_predictions"] ,stats["missed_predictions"],round(sum(stats["alarm time ahead"])/len(stats["alarm time ahead"]),2)))
-        f.write("TPR: {} FPR: {} MPR: {} Avg Forecast: {} \n ".format(str(stats["correct_prediction_percent"]), str(stats["false_predictions"]),str(stats["missed_predictions"]),str(round(sum(stats["alarm time ahead"])/len(stats["alarm time ahead"]),2))))
+        f.write("TPR: {} FPR: {} MPR: {} Avg Forecast: {} \n".format(str(stats["correct_prediction_percent"]), str(stats["false_predictions"]),str(stats["missed_predictions"]),str(round(sum(stats["alarm time ahead"])/len(stats["alarm time ahead"]),2))))
     else:
         print(("TPR: %f FPR: %f MPR: %f Avg Forecast: N/A ") %( stats["correct_prediction_percent"], stats["false_predictions"] ,stats["missed_predictions"])) 
-        f.write("TPR: {} FPR: {} MPR: {} Avg Forecast: N/A \n ".format(str(stats["correct_prediction_percent"]), str(stats["false_predictions"]),str(stats["missed_predictions"])))
+        f.write("TPR: {} FPR: {} MPR: {} Avg Forecast: N/A \n".format(str(stats["correct_prediction_percent"]), str(stats["false_predictions"]),str(stats["missed_predictions"])))
     
     f.close()  
     return stats
