063b21d623327f1768d331c3003a51feb66d6461
yangy96 <yangy96@stanford.edu>
2022-01-26 14:20:10 -0500

    minor change

--- carla_adversarial_experiments/run_sticker_experiments.sh
+++ carla_adversarial_experiments/run_sticker_experiments.sh
@@ -1,3 +1,5 @@
+rm -f ./results/carla_sticker_exp_results.txt
+
 python3 main.py --predict_carla True --initial_memory_threshold 0.5 --memory_dir ./memories/carla_adv_memories_10_0.5 --test_carla_dir ./test_attacks \
         --prob_threshold 0.05 --window_size 5 --window_threshold 5
 
